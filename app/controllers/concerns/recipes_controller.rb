bca4f736dd522bc581a71477723dc8a1dd18392f
mrstain <ferenc.matej@gmail.com>
2015-10-22 22:43:07 +0200

    created new and partials

--- app/controllers/concerns/recipes_controller.rb
+++ app/controllers/concerns/recipes_controller.rb
@@ -14,9 +14,10 @@ class RecipesController < ApplicationController
   end
   
   def create
-    # binding.pry # you can se what is in params
+    #binding.pry # you can se what is in params
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
+    @recipe.chef = Chef.find(1)
+
     
     if @recipe.save
       flash[:success] = "Your recipe was created succesfully"
@@ -24,6 +25,21 @@ class RecipesController < ApplicationController
     else 
       render :new
     end
+  end
+
+  def edit 
+    @recipe =Recipe.find(params[:id])
+
+  end
+
+  def update
+    @recipe =Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated succesfully"
+      redirect_to recipe_path(@recipe)
+    else 
+      render :edit
+    end 
   end
   
   private 
