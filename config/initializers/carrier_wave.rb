8a77d887d9ebc5ad6dbd0060691cddde5eee2f16
mrstain <ferenc.matej@gmail.com>
2015-10-27 12:27:18 +0100

    change vars fro AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
 	Carrier.wave.confugre do |config|
 		config.fog_credentials = {
 			:provider => 'AMS'
-			:aws_acces_key_id => ENV['S3_ACCESS_KEY']
+			:aws_access_key_id => ENV['S3_ACCESS_KEY']
 			:aws_seccret_access_key => ENV['S3_SECRET_KEY']
 		}
 	config.fog.directory = ENV['S3_BUCKET']
