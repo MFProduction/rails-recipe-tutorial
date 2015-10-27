518b77c973ab65b7e637a040fc0e0cc5f27b186d
mrstain <ferenc.matej@gmail.com>
2015-10-27 13:10:05 +0100

    change vars for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,8 @@ if Rails.env.production?
 			#:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 			#:aws_secret_access_key => ENV['S3_SECRET_KEY']
 		}
-		config.fog_directory = ENV['S3_BUCKET']
+		#config.fog_directory = ENV['S3_BUCKET'] railsrecipeimageheroku
+		config.fog_directory = 'railsrecipeimageheroku'
+
 	end
 end
