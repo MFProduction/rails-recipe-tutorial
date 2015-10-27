a0d3db35ec394e06c8f6c6e0586d241c07adbf20
mrstain <ferenc.matej@gmail.com>
2015-10-27 12:31:44 +0100

    change vars for AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
 
-	CarrierWave.confiugre do |config|
+	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			:provider => 'AMS',
 			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
